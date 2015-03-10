36058a8e9fb51ba64e0d263920a4d0ee054036e5
wtfil <evgen.filatov@gmail.com>
2015-03-10 10:41:00 +0200

    fixed time

--- public/components/player-time.js
+++ public/components/player-time.js
@@ -1,14 +1,14 @@
 var React = require('react');
 var Progress = require('./progress');
 function formatTime(time) {
-	var minutes = (time / 60).toFixed(0);
-	var seconds = (time % 60).toFixed(0);
+	var minutes = ~~(time / 60)
+	var seconds = ~~(time % 60);
 	if (minutes >= 1) {
-		minutes = minutes >= 10 ? minutes : ('0' + minutes);
+		minutes = minutes >= 10 ? String(minutes) : ('0' + minutes);
 	} else {
 		minutes = '00';
 	}
-	seconds = seconds >= 10 ? seconds : ('0' + seconds);
+	seconds = seconds >= 10 ? String(seconds) : ('0' + seconds);
 	return minutes + ':' + seconds;
 }
 
